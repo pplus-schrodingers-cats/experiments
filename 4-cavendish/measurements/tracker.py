4ae894fb68a2391eb0ef8662a606b78bd3fd43af
fflopsi <florian.l.frauenfelder@gmail.com>
2025-05-03 14:48:47 +0200

    Add code to calculate G

--- 4-cavendish/measurements/tracker.py
+++ 4-cavendish/measurements/tracker.py
@@ -8,6 +8,7 @@ from PIL import ImageOps, ImageEnhance
 from PIL.ExifTags import TAGS
 from datetime import datetime
 import matplotlib.pyplot as plt
+from uncertainties import ufloat, umath
 
 def load_band(filename, laser_height, band_width):
   '''Return the important part of the image: a slice at laser_height +- band_width, only green channel'''
@@ -66,63 +67,96 @@ def calculate_x_coordinates(series, pixel_coords=np.array([56, 2307, 4056]), met
   series_meters[:, 1] = model(series_meters[:, 1], a_opt, b_opt, c_opt)
   return series_meters
 
-def model(t, A, omega, phase, offset):
-  '''Expected sine wave model'''
-  return A * np.sin(omega * t + phase) + offset
+def perform_measurement(measurement):
+  '''Performs all necessary file operations and calculations, returns 2*n array containing times and meter coordinates'''
+  # Arguments needed for pixel to meter conversion:
+    # Folder name
+    # Laser height in pixel
+    # Reference pixel coordinates
+    # Respective reference meter coordinates (at least three pairs of pixel-meter reference points are required, but the more the better)
+  args = {
+    # '1': ['./measurement1', 1350, 1.18, 2.5],
+    # '2a': ['./measurement2a', 1200, 0.965, 2.16],
+    # '2b': ['./measurement2b', 1220, 0.975, 2.16],
+    '3a': ['./measurement3a', 760,
+      [60, 568, 1089, 1573, 2019, 2435, 2828, 3193, 3536, 3862, 4080],
+      [1.51, 1.6, 1.7, 1.8, 1.9, 2, 2.1, 2.2, 2.3, 2.4, 2.47],
+    ],
+    '3b': ['./measurement3b', 910,
+      [40, 396, 947, 1451, 1913, 2339, 2735, 3102, 3444, 3765, 4066],
+      [1.54, 1.6, 1.7, 1.8, 1.9, 2, 2.1, 2.2, 2.3, 2.4, 2.5],
+    ],
+  }
 
-# The tracker works by sorting all the images in a folder, then taking a thin slice (containing the laserpointer) of each image and looking for the brightest spot (in the green channel)
+  filename = f'measurement{measurement}'
+  if os.path.isfile(f'{filename}.npy'):
+    t, x = np.load(f'{filename}.npy')
+  else:
+    series = create_time_series(
+      directory=args[measurement][0],
+      laser_height=args[measurement][1],
+      band_width=50,
+    )
+    # WARNING: this might take a few minutes every time it's executed
 
-# Tutorial for a new measurement:
-# - pick directory of the measurement and write that into the 'directory' keyword below
-# - determine the y-position of the laser and write the value into the keyword argument 'laser_height' in 'create_time_series' below
-#   (if needed adjust the width of the band around which it cuts but ±50 pixels should be fine)
-# - determine some values on the measuring tape with fitting horizontal pixel values and use them for 'pixel_coords' and 'meter_coords' (more is better, but at least three are required)
+    t, x = calculate_x_coordinates(
+      series,
+      pixel_coords=args[measurement][2],
+      meter_coords=args[measurement][3],
+    ).T
+    np.save(filename, np.array([t, x]))
+  return measurement, t, x
 
-# Use the images from here: https://drive.google.com/drive/folders/1Izo4OTF3CGaBjcfKXcR-0cL6bZGwFbdK and unpack them into a folder named according to the measurement
-args = {
-  # '1': ['./measurement1', 1350, 1.18, 2.5],
-  # '2a': ['./measurement2a', 1200, 0.965, 2.16],
-  # '2b': ['./measurement2b', 1220, 0.975, 2.16],
-  '3a': ['./measurement3a', 760,
-    [60, 568, 1089, 1573, 2019, 2435, 2828, 3193, 3536, 3862, 4080],
-    [1.51, 1.6, 1.7, 1.8, 1.9, 2, 2.1, 2.2, 2.3, 2.4, 2.47],
-  ],
-  '3b': ['./measurement3b', 910,
-    [40, 396, 947, 1451, 1913, 2339, 2735, 3102, 3444, 3765, 4066],
-    [1.54, 1.6, 1.7, 1.8, 1.9, 2, 2.1, 2.2, 2.3, 2.4, 2.5],
-  ],
-}
+def fit_model(measurement, t, x, plot=False):
+  '''Return angular frequency and equilibrium position of sine wave fitted to t, x'''
+  def model(t, A, omega, phase, offset):
+    '''Expected sine wave model'''
+    return A * np.sin(omega * t + phase) + offset
+  # Fit sine wave
+  guess = [0.6, 2*np.pi/500, 3*np.pi/2, 1.97]
+  (A_fit, omega_fit, phase_fit, offset_fit), pcov = sp.optimize.curve_fit(model, t, x, p0=guess)
+  # Uncertainties
+  _, omega_fit_std, _, offset_fit_std = np.sqrt(np.diag(pcov))
+  omega, offset = [ufloat(omega_fit, omega_fit_std), ufloat(offset_fit, offset_fit_std)]
+  print(f'Measurement {measurement}: omega: {omega}/s, equilibrium: {offset}m')
+  if plot:
+    plt.plot(t, x, label='Measured')
+    plt.plot(t, model(t, A_fit, omega_fit, phase_fit, offset_fit), label='Fitted')
+    plt.title(f'Measurement {measurement}')
+    plt.xlabel('Time [s]')
+    plt.ylabel('X [m]')
+    plt.legend()
+    plt.show()
+    # Plot Fourier transform of the measurement
+    # plt.semilogy(np.fft.rfftfreq(len(t), d=(t[-1] - t[0])/len(t)), np.abs(np.fft.rfft(x)))
+    # plt.xlim(-0.003, 0.003)
+    # plt.show()
+  return omega, offset
 
-# Choose your measurement here and execute
-measurement = '3a'
+def calculate_G(omega, eq1, eq2):
+  '''Return a ufloat for the calculated value for the gravitational constant from omega and the two equilibrium positions'''
+  # Some measurements with errors
+  mirror = ufloat(2.04, 0.005)
+  mirror_wall = ufloat(1.885, 0.005)
+  L = ufloat(1.1, 0.0005) # Distance between the two pendulum masses
+  r = ufloat(0.1225, 0.005) # Distance pendulum mass to secondary mass # TODO
+  x = y = ufloat(0.1, 0.0005)
+  z = ufloat(0.102, 0.0005)
+  rho = ufloat(11348, 0.5)
+  M = x*y*z * rho
+  print(f'M = {M} kg')
+  # Calculate the deflection angle
+  theta_1 = 1/2 * umath.atan(umath.fabs(eq1 - mirror) / mirror_wall)
+  theta_2 = 1/2 * umath.atan(umath.fabs(eq2 - mirror) / mirror_wall)
+  theta = umath.fabs(theta_1 - theta_2)
+  print(f'theta = {theta}')
+  # Gravitational constant
+  G = omega**2 * L * r**2 * theta / (2 * M)
+  return G
 
-series = create_time_series(
-  directory=args[measurement][0],
-  laser_height=args[measurement][1],
-  band_width=50,
-)
-# WARNING: this might take a few minutes every time it's executed
+# Use the images from here: https://drive.google.com/drive/folders/1Izo4OTF3CGaBjcfKXcR-0cL6bZGwFbdK and unpack them into a folder with the same name as the zip
 
-t, x = calculate_x_coordinates(
-  series,
-  pixel_coords=args[measurement][2],
-  meter_coords=args[measurement][3],
-).T
-
-# Fit sine wave
-guess = [0.6, 2*np.pi/500, 3*np.pi/2, 1.97]
-(A_fit, omega_fit, phase_fit, offset_fit), _ = sp.optimize.curve_fit(model, t, x, p0=guess)
-print(f'Measurement {measurement}: omega: {omega_fit}/s, equilibrium: {offset_fit}m')
-
-# Plot measurement and fit
-plt.plot(t, x, label='Measured')
-plt.plot(t, model(t, A_fit, omega_fit, phase_fit, offset_fit), label='Fitted')
-plt.xlabel('Time [s]')
-plt.ylabel('X [m]')
-plt.legend()
-plt.show()
-
-# Plot Fourier transform of the measurement
-# plt.semilogy(np.fft.rfftfreq(len(t), d=(t[-1] - t[0])/len(t)), np.abs(np.fft.rfft(x)))
-# plt.xlim(-0.003, 0.003)
-# plt.show()
+w1, e1 = fit_model(*perform_measurement('3a'))
+w2, e2 = fit_model(*perform_measurement('3b'))
+G = calculate_G(w1, e1, e2)
+print(f'G = {G} m^3 / kg s^2')
